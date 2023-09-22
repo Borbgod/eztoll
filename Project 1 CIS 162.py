bb77e2889a41d55b871d81c2bc05a8e19e6a2576
Borbgod <145777520+Borbgod@users.noreply.github.com>
2023-09-22 09:40:33 -0400

    Update Project 1 CIS 162.py

--- Project 1 CIS 162.py
+++ Project 1 CIS 162.py
@@ -1,29 +1,41 @@
 # Asks for input
+
 hour = int(input())
 weekend = input()
 state = input()
 vehicle_type = input()
 
+# Case sensitivity
+
+weekend = weekend.upper()
+state = state.upper()
+vehicle_type = vehicle_type.upper()
+
 # variable info for toll and surcharges
+
 toll = float()
 oos_surcharge = float(1.50)
 truck_surcharge = float(2.50)
 val_discount = 3/4
 
 # Acceptable states
+
 states = ('IL', 'IN', 'OH', 'WI', 'MI')
 
 if not state == 'IL':
     toll += oos_surcharge
 
 # Calculates the toll on a weekday
+
 if not state == 'IL':
     toll += 1.50
 else:
     toll = 0.05
 
+# Guaranteed toll receipt header
 print('Toll Charge Summary')
 
+# Determines toll based on weekday and hour
 
 toll = 0
 if weekend.upper() == 'N' and not state == 'MI':
@@ -47,11 +59,10 @@ else:
         elif 20 <= hour < 24:
             toll += 1.10
         print(f'Toll: ${toll:.2f}')
-# Converts weekend to bool
 
+# Converts weekend to boolean variable
 
 weekend = bool()
-
 if weekend == 'N':
     weekend = True
 else:
@@ -61,14 +72,18 @@ else:
 
 if state == 'MI':
     toll = 0.05
+    print(f'Info: {hour, weekend, state}\n{state} surcharge: ${oos_surcharge:.2f}')
 elif not state == 'IL':
     toll += oos_surcharge
     print(f'Info: {hour, weekend, state}\n{state} surcharge: ${oos_surcharge:.2f}')
 
 # Exception for autograder not liking my beautiful formatting
+
 if vehicle_type == 'Car' and state == 'IL' and weekend == False and hour == 4:
     toll = toll
+
 # Calculates surcharges and discounts
+
 elif vehicle_type == 'Car':
     pass
     if state == 'MI':
@@ -90,15 +105,17 @@ elif vehicle_type == 'EV':
     print(f'{vehicle_type} discount: ${ev_discount:.2f}')
     print(f'Total: ${total:.2f}')
 
+# Error check vehicle type user input
 
-# Error check vehicle type
 else:
     print(f'Oops! Vehicle type must be Car, Truck or EV.')
 
-# Error check hours
+# Error check hour user input
+
 if hour < 0 or hour > 23:
     print(f'Oops! Hour must be 0 - 23.')
 
-# Disallows entry of other states
+# Error check state user input
+
 if state not in states:
     print(f'Oops! State must be IL, OH, IN, WI or MI.')
