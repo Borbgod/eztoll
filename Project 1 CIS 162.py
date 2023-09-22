8c43ef1614927699d097f778a3bc2cf36584d65c
Borbgod <145777520+Borbgod@users.noreply.github.com>
2023-09-22 14:32:52 -0400

    Update Project 1 CIS 162.py

--- Project 1 CIS 162.py
+++ Project 1 CIS 162.py
@@ -4,35 +4,29 @@ hour = int(input())
 weekend = input()
 state = input()
 vehicle_type = input()
-
+addons = bool()
 # Case sensitivity
 
 weekend = weekend.upper()
 state = state.upper()
 
-# variable info for toll and surcharges
+# Variable info for toll and surcharges
 
-toll = float()
-oos_surcharge = float(1.50)
-truck_surcharge = float(2.50)
+toll = float(0)
 val_discount = 3/4
-
+oos_surcharge = float(0)
+truck_surcharge = float(0)
+ev_discount = 0
+total = 0
 # Acceptable states
 
 states = ('IL', 'IN', 'OH', 'WI', 'MI')
 
-if not state == 'IL':
-    toll += oos_surcharge
-
-# Calculates the toll on a weekday
 
 if not state == 'IL':
     toll += 1.50
 else:
     toll = 0.05
-
-# Guaranteed toll receipt header
-print('Toll Charge Summary')
 
 # Determines toll based on weekday and hour
 
@@ -48,7 +42,6 @@ if weekend.upper() == 'N' and not state == 'MI':
         toll += 3.45
     elif 20 <= hour < 24:
         toll += 1.15
-    print(f'Toll: ${toll:.2f}')
 else:
     if weekend.upper() == 'Y' and not state == 'MI':
         if hour < 7:
@@ -57,7 +50,6 @@ else:
             toll += 1.75
         elif 20 <= hour < 24:
             toll += 1.10
-        print(f'Toll: ${toll:.2f}')
 
 # Converts weekend to boolean variable
 
@@ -71,44 +63,47 @@ else:
 
 if state == 'MI':
     toll = 0.05
-    print(f'Info: {hour, weekend, state}')
 elif not state == 'IL':
-    toll += oos_surcharge
-    print(f'Info: {hour, weekend, state}\n{state} surcharge: ${oos_surcharge:.2f}')
+    oos_surcharge = float(1.50)
+    total = + toll + oos_surcharge
 
-# Exception for autograder not liking my beautiful formatting
+# Guaranteed toll receipt header
 
-if vehicle_type == 'Car' and state == 'IL' and weekend == False and hour == 4:
-    toll = toll
+print('Toll Charge Summary')
+print(f'Info: {hour, weekend, state}')
+print(f'Toll: ${toll:.2f}')
 
 # Calculates surcharges and discounts
-
-elif vehicle_type == 'Car':
+if vehicle_type == 'Car':
     pass
     if state == 'MI':
         toll = 0.05
-    print(f'Total: ${toll:.2f}')
 elif vehicle_type == 'Truck' and state == 'MI':
     toll = 0.05
-    print(f'Toll: ${toll:.2f}')
 elif vehicle_type == 'Truck':
-    toll += truck_surcharge
-    print(f'{vehicle_type} surcharge: ${truck_surcharge:.2f}\nTotal: ${toll:.2f}')
-    print(f'Toll: ${toll:.2f}')
+    truck_surcharge = float(2.50)
+    total += truck_surcharge
 elif vehicle_type == 'EV' and state == 'MI':
     toll = 0.05
-    print(f'Toll: ${toll:.2f}')
 elif vehicle_type == 'EV':
-    ev_discount = toll * 1/4
-    toll *= val_discount
-    total = toll
-    print(f'{vehicle_type} discount: ${ev_discount:.2f}')
-    print(f'Total: ${total:.2f}')
-
-# Error check vehicle type user input
-
+    ev_discount = total * 0.25
+    total -= ev_discount
 else:
     print(f'Oops! Vehicle type must be Car, Truck or EV.')
+
+
+# Fixes surcharges
+
+if oos_surcharge > 0:
+    print(f'{state} surcharge: ${oos_surcharge:.2f}')
+    if vehicle_type == 'Car':
+        print(f'Total: ${total:.2f}')
+if truck_surcharge > 0:
+    print(f'{vehicle_type} surcharge: ${truck_surcharge:.2f}\nTotal: ${total:.2f}')
+    print(f'Total: ${total:.2f}')
+elif ev_discount > 0:
+    print(f'{vehicle_type} discount: ${ev_discount:.2f}')
+    print(f'Total: ${total:.2f}')
 
 # Error check hour user input
 
@@ -119,7 +114,3 @@ if hour < 0 or hour > 23:
 
 if state not in states:
     print(f'Oops! State must be IL, OH, IN, WI or MI.')
-
-addons = (oos_surcharge, truck_surcharge, ev_discount)
-if addons:
-    print()
